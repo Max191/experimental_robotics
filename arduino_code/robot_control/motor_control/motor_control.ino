d6f890da409f5ac614ecbdf9cf4c251238db2535
Max191 <max.dawkins@gmail.com>
2022-10-13 00:13:23 -0400

    robot control nodes

--- arduino_code/robot_control/motor_control/motor_control.ino
+++ arduino_code/robot_control/motor_control/motor_control.ino
@@ -4,6 +4,10 @@ This code uses an Arduino uno to command one SMD PWM Motor COntroller (TE-294-00
 
 
 ***********************************************************/
+#include <geometry_msgs/Twist.h>
+#include <ros.h>
+#include <stdlib.h>
+
 /*right side*/
 const int inAPinRight = 4; 
 const int inBPinRight = 2;
@@ -14,64 +18,66 @@ const int inAPinLeft = 8;
 const int inBPinLeft = 7;
 const int PWMPinLeft = 9;
 
+bool left_moving_forward = false;
+bool left_moving_backward = false;
+bool right_moving_forward = false;
+bool right_moving_backward = false;
+
+ros::NodeHandle  nh;
+
+ros::Subscriber<geometry_msgs::Twist> wheel_control_sub("/robot_control/right_left_speed_cmd", wheel_control);
+
+void wheel_control( const geometry_msgs::Twist& cmd_msg) {
+   int right_cmd = (int)(cmd_msg.linear.x);
+   int left_cmd = (int)(cmd_msg.angular.z);
+
+   //set wheel directions
+   if(!left_moving_forward && left_cmd >= 0.f){
+      digitalWrite(inAPinLeft, LOW); // move one direction
+      digitalWrite(inBPinLeft, HIGH);
+      left_moving_forward = true;
+      left_moving_backward = false;
+   }
+   if(!left_moving_backward && left_cmd < 0.f){
+      digitalWrite(inAPinLeft, HIGH); // move other direction
+      digitalWrite(inBPinLeft, LOW);
+      left_moving_backward = true;
+      left_moving_forward = false;
+   }
+   if(!right_moving_forward && right_cmd >= 0.f){
+      digitalWrite(inAPinRight, HIGH); // move one direction
+      digitalWrite(inBPinRight, LOW);
+      right_moving_forward = true;
+      right_moving_backward = false;
+   }
+   if(!right_moving_backward && right_cmd < 0.f){
+      digitalWrite(inAPinRight, LOW); // move other direction
+      digitalWrite(inBPinRight, HIGH);
+      right_moving_backward = true;
+      right_moving_forward = false;
+   }
+   delay(10);
+
+   analogWrite(PWMPinRight, abs(right_cmd));
+   analogWrite(PWMPinLeft, abs(left_cmd));
+}
+
 void setup() {
 
-  pinMode(inAPinRight, OUTPUT);
-  pinMode(inBPinRight, OUTPUT);
-  pinMode(PWMPinRight, OUTPUT);
+   pinMode(inAPinRight, OUTPUT);
+   pinMode(inBPinRight, OUTPUT);
+   pinMode(PWMPinRight, OUTPUT);
 
-  pinMode(inAPinLeft, OUTPUT);
-  pinMode(inBPinLeft, OUTPUT);
-  pinMode(PWMPinLeft, OUTPUT);
+   pinMode(inAPinLeft, OUTPUT);
+   pinMode(inBPinLeft, OUTPUT);
+   pinMode(PWMPinLeft, OUTPUT);
+
+   nh.initNode();
+
+   nh.subscribe(wheel_control_sub);
 }
 
 void loop() {
-  int i;
-  digitalWrite(inAPinRight, LOW); //stop motors can be both low or high
-  digitalWrite(inBPinRight, LOW);
-  digitalWrite(inAPinLeft, LOW); //stop motors can be both low or high
-  digitalWrite(inBPinLeft, LOW);
-  
-  delay(500);
-  digitalWrite(inAPinRight, HIGH); // move one direction
-  digitalWrite(inBPinRight, LOW);
-  digitalWrite(inAPinLeft, LOW); // move one direction
-  digitalWrite(inBPinLeft, HIGH);
-  
-  for (i = 0; i < 200; i++){ 
-     analogWrite(PWMPinRight, i);
-     analogWrite(PWMPinLeft, i);
-     delay(10);
-  }
-  for (i = 199; i >= 0; i--){ 
-     analogWrite(PWMPinRight, i);
-     analogWrite(PWMPinLeft, i);
-     delay(10);
-  }
-   delay(500);
-
-  digitalWrite(inAPinRight, LOW); // stop motor
-  digitalWrite(inBPinRight, LOW);
-  digitalWrite(inAPinLeft, LOW); // stop motor
-  digitalWrite(inBPinLeft, LOW);
-  delay(500);
-  
-  digitalWrite(inAPinRight, LOW); // move other direction
-  digitalWrite(inBPinRight, HIGH);
-  digitalWrite(inAPinLeft, HIGH); // move other direction
-  digitalWrite(inBPinLeft, LOW);
-  
-  for (i = 0; i < 200; i++){
-     analogWrite(PWMPinRight,i);
-     analogWrite(PWMPinLeft,i);
-     delay(10);
-  }
-  for (i = 199; i >= 0; i--){ 
-     analogWrite(PWMPinRight, i);
-     analogWrite(PWMPinLeft, i);
-     delay(10);
-  }
-   delay(500);
-   
-
+   nh.spinOnce();
+   delay(1);
 }
